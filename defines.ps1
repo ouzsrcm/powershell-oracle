c7aba9329fbf0e3bb7c21d41f622a498db7b3fbc
ouzsrcm <oguzhan.saricam@icloud.com>
2020-11-24 17:45:09 +0300

    config dosyası

--- defines.ps1
+++ defines.ps1
@@ -3,9 +3,37 @@ add-type -AssemblyName System.Data.OracleClient
 #scripts base directory
 $dir = "./temp/db";
 
-$username = "h4a"
-$password = "h4a"
-$data_source = "h4a_mapfre"
+$global:appSettings = @{}
+
+$configFile = "./ps.config"
+
+if (Test-Path $configFile) {
+    Try {
+        $config = [xml](get-content $configFile)
+        foreach ($addNode in $config.configuration.appsettings.add) {
+            if ($addNode.Value.Contains(',')) {
+                $value = $addNode.Value.Split(',')
+                for ($i = 0; $i -lt $value.length; $i++) { 
+                    $value[$i] = $value[$i].Trim()
+                }
+            }
+            else {
+                $value = $addNode.Value
+            }
+            $global:appSettings[$addNode.Key] = $value
+        }
+    }
+    Catch [system.exception] {
+        
+    }
+}
+else {
+    Write-Error -Message "Config dosyası okunamadı." -Category ReadError
+}
+
+$username = $global:appSettings["username"]
+$password = $global:appSettings["password"]
+$data_source = $global:appSettings["data_source"]
 $connection_string = "User Id=$username;Password=$password;Data Source=$data_source"
 
 $objects = @(
